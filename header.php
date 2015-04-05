c6865ad17ebc1a95a44c42ac012d1ac7a5b584ef
eamania <eamania02@gmail.com>
2015-04-05 10:56:00 +0200

    social link megamenu

--- header.php
+++ header.php
@@ -47,21 +47,7 @@
 								<div class="col-md-12">
 									<div class="mega-menu-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="logo-cl.png" height="60px" /><span class="hidden-sm hidden-xs"><?php bloginfo( 'name' ); ?></span></a></div>				
 									<span id="mega-menu-prymary-big"><?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></span>
-									<div id="social-link-search" class="mega-menu-social-div">
-										<div class="mega-menu-social-link pull-left">
-											<i class="fa fa-facebook fa-3x"></i>
-											<i class="fa fa-twitter fa-3x"></i>
-											<i class="fa fa-flickr fa-3x"></i>
-											<i class="fa fa-instagram fa-3x"></i>
-										</div>
-										<a id="slick-slidetoggle" class="mega-menu-search-icon pull-left" href="#"><span class="glyphicon glyphicon-search"></span></a></h3>
-										<div id="mega-menu-search-id" class="mega-menu-search pull-left">
-											<form method="get" id="search_form" action="<?php bloginfo('home'); ?>"/>
-												<input type="text" class="text" name="s" value="cerca" >
-												<input type="submit" class="submit glyphicon glyphicon-search " value="Cerca"  />
-											</form>
-										</div>
-									</div>
+									<?php get_template_part("partial/megamenu" , "social-search" );?>
 								</div>
 							</div>
 						</div>
---
+++ partial/megamenu-social-search.php
@@ -0,0 +1,17 @@
+<div id="social-link-search" class="mega-menu-social-div">
+	<div class="mega-menu-social-link pull-left">
+		<a href=""><i class="fa fa-facebook fa-2x"></i></a>
+		<a href=""><i class="fa fa-twitter fa-2x"></i></a>
+		<a href=""><i class="fa fa-flickr fa-2x"></i></a>
+		<a href=""><i class="fa fa-instagram fa-2x"></i></a>
+		<a href=""><i class="fa fa-ellipsis-v fa-2x"></i></a>
+		<a id="slick-slidetoggle" href="#"><i class="fa fa-search fa-2x"></i></a>
+
+	</div>
+	<div id="mega-menu-search-id" class="mega-menu-search pull-left">
+		<form method="get" id="search_form" action="<?php bloginfo('home'); ?>"/>
+			<input type="text" class="text" name="s" value="cerca" >
+			<input type="submit" class="submit glyphicon glyphicon-search " value="Cerca"  />
+		</form>
+	</div>
+</div>
