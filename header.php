2f7668c829ea665536846473be3573b35dda913c
eamania <eamania02@gmail.com>
2015-09-27 10:39:13 +0200

    aggiunta backup datase

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head> v
+<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
