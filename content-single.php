c00185f30c8713314440d3283c11f37351b45f46
eamania <eamania02@gmail.com>
2015-10-05 16:52:22 +0200

    05/10/2015

--- content-single.php
+++ content-single.php
@@ -10,8 +10,10 @@
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header class="entry-header">
                 <div id="content-article-header">
+
+                    <?php the_post_thumbnail('thumbnails_article_big', array('class' => "img-responsive")); ?>
                     <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                    <?php the_post_thumbnail('thumbnails_article_big'); ?>
+
                     <?php miraforever_posted_on(); ?>
                 </div>
                 <div class="entry-meta">
--- partial/share/article-header.php
+++ partial/share/article-header.php
@@ -6,6 +6,7 @@
 
 
 
-<div id="article-header" class="" style="height: 330px; background-color: red;">
+<div id="article-header" class="" style="height: 400px; width: 100%; background-image: url('http://localhost/miraforever/attrazioni.jpg');  background-position: center center; background-size: 100% auto;">
 
 </div>
+
